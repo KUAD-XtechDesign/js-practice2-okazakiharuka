ef54c4fd524dd6ca2838eba127fa0e7f5ebf0a22
okazakiharuka <h-okazaki6276@st.kyoto-art.ac.jp>
2021-10-15 15:18:39 +0900

    text

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,30 @@
 $(function(){
 
+  let hako = 0;
+
+  function checkBox(){
+    if (hako > 5){
+      $("#content").text(hako + "個入っています。満杯です。");
+      hako = 0;
+    } else if (hako <= 0){
+      hako = 0;
+      $("#content").text(hako + "個入っています。空です。");
+    } else {
+      $("#content").text(hako + "個入っています。");
+    }
+  }
+
+  $("#button01").on('click', function(){
+    checkBox();
+  })
+  $("#button02").on('click', function(){
+    hako = hako + 1;
+    checkBox();
+  })
+  $("#button03").on('click', function(){
+    hako = hako - 1;
+    checkBox();
+  })
 
 
 })
